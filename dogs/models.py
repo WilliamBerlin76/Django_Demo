a9e1a57dc2ddbeb072df7c55d312e7c7aa69713c
WilliamBerlin76 <william.berlin76@gmail.com>
2020-04-17 14:54:23 -0500

    updated migrations

--- dogs/models.py
+++ dogs/models.py
@@ -4,5 +4,7 @@ from uuid import uuid4
 class Dog(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=200)
+    description = models.CharField(blank=True, max_length=1000)
     breed = models.CharField(max_length=200)
     weight = models.IntegerField()
+    friendly = models.BooleanField(default=True)
