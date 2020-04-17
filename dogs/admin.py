2901aecd28f4aeddfcb98b642bb0645f4e2ccf3c
WilliamBerlin76 <william.berlin76@gmail.com>
2020-04-17 16:11:44 -0500

    added a few subclasses

--- dogs/admin.py
+++ dogs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Dog
+from .models import Dog, Dogs_Owner
 
 admin.site.register(Dog)
+admin.site.register(Dogs_Owner)
--- dogs/models.py
+++ dogs/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from uuid import uuid4
 
+from django.contrib.auth.models import User
+
 class Dog(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=200)
@@ -8,3 +10,6 @@ class Dog(models.Model):
     breed = models.CharField(max_length=200)
     weight = models.IntegerField()
     friendly = models.BooleanField(default=True)
+
+class Dogs_Owner(Dog):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
