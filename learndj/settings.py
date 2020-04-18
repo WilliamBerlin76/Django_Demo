26b04049cf8803e8acb1f0bc2826531776362c8f
WilliamBerlin76 <william.berlin76@gmail.com>
2020-04-18 15:43:48 -0500

    switched from sqlite to local postgres

--- learndj/settings.py
+++ learndj/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'learndj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_demo_postgres',
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost'
     }
 }
 
